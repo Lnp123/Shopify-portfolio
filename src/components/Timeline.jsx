13d5950cc9a8d9e773f23003603b97aa4ec6c675
Robin Ibañez <93378496+Lnp123@users.noreply.github.com>
2026-04-21 16:57:14 +0800

    Update Timeline.jsx

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -8,7 +8,7 @@ function Timeline() {
       <div className="flex flex-col md:flex-row justify-center my-20">
          <div className="w-full md:w-7/12">
             <Title>Work Experience</Title>
-            {timeline.map(item => (
+            {Timeline.map(item => (
                <TimelineItem 
                   year={item.year}
                   title={item.title}
