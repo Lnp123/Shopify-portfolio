0765bc217df149e2c3b342ed7956b9c039bc61bb
Robin Ibañez <93378496+Lnp123@users.noreply.github.com>
2026-04-21 16:49:54 +0800

    Update timeline.js

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,34 +1,34 @@
-// export default [
-// 	{
-// 	   year: 'February - April 2023',
-// 	   title: 'Internship',
-// 	   duration: '3 months',
-// 	   details: 
-// 		  'Web Developer we created a Ticketing System also im helping for UI design.',
-// 	},
-// 	{
+export default [
+	{
+	   year: 'February - April 2023',
+	   title: 'Internship',
+	   duration: '3 months',
+	   details: 
+		  'Web Developer we created a Ticketing System also im helping for UI design.',
+	},
+	{
 		 
-//                  year: 'December 2023 - April 2024',
-// 		 title: 'Datablitz - Web Developer',
-// 		 duration: '5 months',
-// 		 details:
-// 			 "Responsible for maintaining the Shopify website and ensuring consistent daily product uploads. Once products are successfully published on the website, they are systematically transferred and listed on marketplaces such as Lazada and Shopee.",
-// 	 },
-// {
-// 		 year: 'May 2024 - September 2024',
-// 		 title: 'Freelance Shopify Web Designer',
-// 		 duration: '5 months',
-// 		 details:
-// 			 "Redesigns client stores to improve visual appeal, user experience, and overall layout. Focuses on creating clean, engaging, and conversion friendly landing pages to enhance customer navigation and boost store performance.",
-// 	 },
+                 year: 'December 2023 - April 2024',
+		 title: 'Datablitz - Web Developer',
+		 duration: '5 months',
+		 details:
+			 "Responsible for maintaining the Shopify website and ensuring consistent daily product uploads. Once products are successfully published on the website, they are systematically transferred and listed on marketplaces such as Lazada and Shopee.",
+	 },
+{
+		 year: 'May 2024 - September 2024',
+		 title: 'Freelance Shopify Web Designer',
+		 duration: '5 months',
+		 details:
+			 "Redesigns client stores to improve visual appeal, user experience, and overall layout. Focuses on creating clean, engaging, and conversion friendly landing pages to enhance customer navigation and boost store performance.",
+	 },
 
-// {
+{
 	        
-//                  year: 'Nov 2024 - Present',
-// 		 title: 'Chrissports - Ecommerce Assistant',	
-// 		 duration: 'Nov 2024 - Present',
-// 		 details:
-// 			 "Manages and maintains Shopify websites by uploading products daily with accurate titles, images, pricing, and variants. Performs theme customization and store redesigns to enhance visual appeal, layout, and user experience. Optimizes product listings using SEO best practices and sets up promotions, discount codes, and sales campaigns aligned with marketing initiatives. Handles API integrations for seamless syncing of product and order data, and cross-lists products to platforms such as Lazada, Shopee and Tiktok.",
-// 	 },
+                 year: 'Nov 2024 - Present',
+		 title: 'Chrissports - Ecommerce Assistant',	
+		 duration: 'Nov 2024 - Present',
+		 details:
+			 "Manages and maintains Shopify websites by uploading products daily with accurate titles, images, pricing, and variants. Performs theme customization and store redesigns to enhance visual appeal, layout, and user experience. Optimizes product listings using SEO best practices and sets up promotions, discount codes, and sales campaigns aligned with marketing initiatives. Handles API integrations for seamless syncing of product and order data, and cross-lists products to platforms such as Lazada, Shopee and Tiktok.",
+	 },
  
-//  ]
+ ]
