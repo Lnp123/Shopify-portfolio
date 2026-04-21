359d76e56764b8fb7a0ed4162d4b4f2939e2b697
Robin Ibañez <93378496+Lnp123@users.noreply.github.com>
2026-04-21 16:49:21 +0800

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,89 +1,89 @@
-// import React from 'react';
+import React from 'react';
 
-// function Intro() {
-//    return (
-//       <div className="flex flex-col items-center justify-center text-center pt-20 pb-6">
-//          <div className="flex flex-col items-center mb-3">
-//             <img
-//                src="/assets/me.jpg"
-//                alt="Profile"
-//                className="w-24 h-24 rounded-full mb-2 md:w-32 md:h-32"
-//             />
-//             <div className="flex flex-col">
-//                <h1 className="text-2xl md:text-4xl dark:text-white mb-1 md:mb-3 font-bold">ROBIN IBAÑEZ</h1>
-//                <p className="text-sm md:text-base mb-3 font-medium">TECH STACKS</p>
-//             </div>
-//          </div>
-//          <div className="flex flex-row">
-//          <a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank" rel="noopener noreferrer">
-//             <img
-//                src="/assets/html.png"
-//                alt="HTML Icon"
-//                className="w-12 h-12 rounded-full mr-2 md:w-16 md:h-16"
-//             />
-//             </a>
-//             <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank" rel="noopener noreferrer">
-//             <img
-//                src="/assets/css.png"
-//                alt="CSS Icon"
-//                className="w-12 h-12 rounded-full mr-2 md:w-16 md:h-16"
-//             />
-//             </a>
-//             <a href="https://tailwindcss.com/" target="_blank" rel="noopener noreferrer">
-//             <img
-//                src="/assets/tailwindcss.png"
-//                alt="tailwind Icon"
-//                className="w-12 h-12 rounded-full mr-2 md:w-16 md:h-16"
-//             />
-//             </a>
-//             <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noopener noreferrer">
-//             <img
-//                src="/assets/js.png"
-//                alt="javascript Icon"
-//                className="w-12 h-12 rounded-full mr-2 md:w-16 md:h-16"
-//             />
-//             </a>
-//             <a href="https://react.dev/" target="_blank" rel="noopener noreferrer">
-//             <img
-//                src="/assets/react.png"
-//                alt="reactjs Icon"
-//                className="w-12 h-12 rounded-full mr-2 md:w-16 md:h-16"
-//             />
-//             </a>
-//             <a href="https://vitejs.dev/" target="_blank" rel="noopener noreferrer">
-//             <img
-//                src="/assets/vitejs.png"
-//                alt="vite Icon"
-//                className="w-12 h-12 rounded-full mr-2 md:w-16 md:h-16"
-//             />
-//             </a>
-//             <a href="https://nodejs.org/en" target="_blank" rel="noopener noreferrer">
-//             <img
-//                src="/assets/nodejs.png"
-//                alt="node Icon"
-//                className="w-12 h-12 rounded-full mr-2 md:w-16 md:h-16"
-//             />
-//             </a>
-//             <a href="https://git-scm.com/" target="_blank" rel="noopener noreferrer">
-//             <img
-//                src="/assets/git.png"
-//                alt="git Icon"
-//                className="w-12 h-12 rounded-full mr-2 md:w-16 md:h-16"
-//             />
-//             </a>
+function Intro() {
+   return (
+      <div className="flex flex-col items-center justify-center text-center pt-20 pb-6">
+         <div className="flex flex-col items-center mb-3">
+            <img
+               src="/assets/me.jpg"
+               alt="Profile"
+               className="w-24 h-24 rounded-full mb-2 md:w-32 md:h-32"
+            />
+            <div className="flex flex-col">
+               <h1 className="text-2xl md:text-4xl dark:text-white mb-1 md:mb-3 font-bold">ROBIN IBAÑEZ</h1>
+               <p className="text-sm md:text-base mb-3 font-medium">TECH STACKS</p>
+            </div>
+         </div>
+         <div className="flex flex-row">
+         <a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank" rel="noopener noreferrer">
+            <img
+               src="/assets/html.png"
+               alt="HTML Icon"
+               className="w-12 h-12 rounded-full mr-2 md:w-16 md:h-16"
+            />
+            </a>
+            <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank" rel="noopener noreferrer">
+            <img
+               src="/assets/css.png"
+               alt="CSS Icon"
+               className="w-12 h-12 rounded-full mr-2 md:w-16 md:h-16"
+            />
+            </a>
+            <a href="https://tailwindcss.com/" target="_blank" rel="noopener noreferrer">
+            <img
+               src="/assets/tailwindcss.png"
+               alt="tailwind Icon"
+               className="w-12 h-12 rounded-full mr-2 md:w-16 md:h-16"
+            />
+            </a>
+            <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noopener noreferrer">
+            <img
+               src="/assets/js.png"
+               alt="javascript Icon"
+               className="w-12 h-12 rounded-full mr-2 md:w-16 md:h-16"
+            />
+            </a>
+            <a href="https://react.dev/" target="_blank" rel="noopener noreferrer">
+            <img
+               src="/assets/react.png"
+               alt="reactjs Icon"
+               className="w-12 h-12 rounded-full mr-2 md:w-16 md:h-16"
+            />
+            </a>
+            <a href="https://vitejs.dev/" target="_blank" rel="noopener noreferrer">
+            <img
+               src="/assets/vitejs.png"
+               alt="vite Icon"
+               className="w-12 h-12 rounded-full mr-2 md:w-16 md:h-16"
+            />
+            </a>
+            <a href="https://nodejs.org/en" target="_blank" rel="noopener noreferrer">
+            <img
+               src="/assets/nodejs.png"
+               alt="node Icon"
+               className="w-12 h-12 rounded-full mr-2 md:w-16 md:h-16"
+            />
+            </a>
+            <a href="https://git-scm.com/" target="_blank" rel="noopener noreferrer">
+            <img
+               src="/assets/git.png"
+               alt="git Icon"
+               className="w-12 h-12 rounded-full mr-2 md:w-16 md:h-16"
+            />
+            </a>
           
-//          </div>
-//          <p className="text-sm max-w-xl mt-6 font-bold">
-//          Hello! My name is Robin Ibañez, and I am thrilled to introduce myself as an entry level front-end developer. 
-//          I have a deep passion for creating visually appealing and user-friendly web experiences that engage and delight users.
+         </div>
+         <p className="text-sm max-w-xl mt-6 font-bold">
+         Hello! My name is Robin Ibañez, and I am thrilled to introduce myself as an entry level front-end developer. 
+         I have a deep passion for creating visually appealing and user-friendly web experiences that engage and delight users.
             
-//             <div className="flex flex-col mt-6 md:mt-8 animate-fade-in">
-//                <p className="text-sm md:text-base mb-3 font-medium">PROJECTS</p>
-//             </div>
+            <div className="flex flex-col mt-6 md:mt-8 animate-fade-in">
+               <p className="text-sm md:text-base mb-3 font-medium">PROJECTS</p>
+            </div>
 
-//          </p>
-//       </div>
-//    )
-// }
+         </p>
+      </div>
+   )
+}
 
-// export default Intro;
+export default Intro;
